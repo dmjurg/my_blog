f9baac8aeaea50ef1617800a7f39614300ac55e4
DIANA JURGIELEWICZ <DMJURG@GMAIL.COM>
2015-04-15 07:25:12 -0400

    w05d02

--- my_blog/db/seeds.rb
+++ my_blog/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Tag.create([
+  {name: "Food & Drink"},
+  {name: "Events"},
+  {name: "Cultural"}
+  ])
+
+Tag.find_by(name: "Food & Drink").posts.create({title: "Right Proper", content: "A charming neighborhood brewpub"})
+
+Tag.find_by(name: "Cultural").posts.create(
+  {title: "The 9:30 Club", content: "One of the greatest music venues on the east coast"},
+  {title: "The National Zoo", content: "Free animals for all"}
+)
